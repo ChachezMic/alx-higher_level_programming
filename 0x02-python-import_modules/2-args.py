047dc7f995f71bd0523fd21feac07a1da9ba49c1
ChachezMic <chachezmic@gmail.com>
2024-02-03 15:15:24 -0800

    fghost

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     else:
         print("{:d} arguments:".format(len(argv)-1))
         for i in range(1, len(argv)):
-            print("{:d}: {} ".format(i, argv[i]))
+            print("{:d}: {}".format(i, argv[i]))
 
