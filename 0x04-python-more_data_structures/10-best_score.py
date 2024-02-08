6a61e1bcd3078b718899c09e647ce3f295839ef9
ChachezMic <chachezmic@gmail.com>
2024-02-07 18:38:55 -0800

    high

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     if a_dictionary:
 
-        sorted_keys = sorted(a_dictionary)
-        highest_score = sorted_keys[-1]
-        return highest_score
+        sorted_keys = sorted(a_dictionary, key=a_dictionary.get)
+        highest_score_key = sorted_keys[-1]
+        return highest_score_key
     return None
