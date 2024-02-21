9a4d1abf0ea41d6324a5e4c35311fe391194d13d
ChachezMic <chachezmic@gmail.com>
2024-02-20 16:16:27 -0800

    mancity

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
-class MyList(list):
-    def print_sorted(self):
+"""
+This module contains class that inherit another class
+and in this case, class list.
+"""
 
+
+class MyList(list):
+    """This class inherits the default list class
+
+    Args:
+        list: The parent or base class
+    Attributes:
+        print_sorted - Print the list in sorted (ascending order)
+        without mutating the original list
+    """
+    def print_sorted(self):
+        """Print list in ascending other without mutating"""
         print(sorted(self))
