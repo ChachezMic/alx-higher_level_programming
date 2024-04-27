6399eba834058d05cf4fd989dbf7c69f252aefa3
ChachezMic <chachezmic@gmail.com>
2024-04-27 05:24:48 -0700

    ho

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+const myC = 'C is fun';
+const myPython = 'Python is cool';
+const myJs = 'JavaScript is amazing';
+
+console.log(myC);
+console.log(myPython);
+console.log(myJs);
