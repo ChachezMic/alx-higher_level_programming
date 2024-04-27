6b0345d212d04e797119f8b5ab417c6235a70276
ChachezMic <chachezmic@gmail.com>
2024-04-27 05:38:28 -0700

    hardtime

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,6 +3,6 @@ const myC = 'C is fun';
 const myPython = 'Python is cool';
 const myJs = 'JavaScript is amazing';
 
-console.log(myC);
-console.log(myPython);
+console.log(myC + "\n");
+console.log(myPython + "\n");
 console.log(myJs);
