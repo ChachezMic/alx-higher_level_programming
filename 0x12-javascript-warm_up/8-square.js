1c7a235ef4db9768a7761394332e479a8deb0ba9
ChachezMic <chachezmic@gmail.com>
2024-04-30 05:07:49 -0700

    well

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
-
 const { argv } = require('process');
 const number = parseInt(argv[2]);
 if (!number) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < number; i++) {
-    let row = ' ';
+    let sum = '';
     for (let j = 0; j < number; j++) {
-      row += 'x';
+      sum += 'X';
     }
-    console.log(row);
+    console.log(sum);
   }
 }
