12527012f28a4e0216f28a3b7afb66fc84195202
ChachezMic <chachezmic@gmail.com>
2024-05-21 04:51:14 -0700

    tofauti

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-""" Define State model """
-from model_state import Base
+"""list all states"""
+
+from sqlalchemy import create_engine, Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import sessionmaker
 
+engine = create_engine('mysql://user:password@localhost:3306/your_database_name')
+Base = declarative_base()
+metadata = MetaData()
 
-class City(Base):
-    """ Define a class State to be linked to db table """
-    __tablename__ = 'cities'
-
-    id = Column(Integer, nullable=False,
-                autoincrement=True, unique=True,
-                primary_key=True)
-
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+states = session.query(State).all()
+for state in states:
+    print(f"State ID: {state.id}, Name: {state.name}")
+
+session.close()
