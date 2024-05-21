6c02a6df8608fb486688f139fed2b5744d975d81
ChachezMic <chachezmic@gmail.com>
2024-05-21 05:52:04 -0700

    6-model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-"""list all states"""
+"""
+contains the class definition of a State and an instance Base
+"""
 
-from sqlalchemy import create_engine, Column, Integer, String, MetaData
+import sqlalchemy
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-engine = create_engine/
-('mysql://user:password@localhost:3306/your_database_name')
 Base = declarative_base()
-metadata = MetaData()
-
 
 class State(Base):
+    """Representation of a state"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-states = session.query(State).all()
-for state in states:
-    print(f"State ID: {state.id}, Name: {state.name}")
-
-session.close()
