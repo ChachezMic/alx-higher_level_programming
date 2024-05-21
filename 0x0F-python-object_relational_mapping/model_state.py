54c695fdcf1d558eb588c2567b1a88011aafe46b
ChachezMic <chachezmic@gmail.com>
2024-05-21 04:57:08 -0700

    tofauti

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,14 +5,17 @@ from sqlalchemy import create_engine, Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql://user:password@localhost:3306/your_database_name')
+engine = create_engine/
+('mysql://user:password@localhost:3306/your_database_name')
 Base = declarative_base()
 metadata = MetaData()
+
 
 class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
